f6aa27e8430bb9ec4ba1b3ef434c5921ddfae490
André Albson <andre.albson@precode.com.br>
2024-05-15 10:22:04 -0300

    Update routing in Vue application
    The routing in the Vue.js application has been updated. The 'home' route was replaced with a 'Create New Employee' route. Additionally, the 'about' route was replaced with a separate 'Edit Employee' and 'List All Employees' routes. These changes are part of an effort to enhance the functionality of the application.

--- front-vue/src/router/index.js
+++ front-vue/src/router/index.js
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'Create New Emplyee',
+    component: () => import('../components/create-employee/CreateEmployeeComponent.vue'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/list-employees',
+    name: 'List All Employees',
+    component: () => import('../components/list-employee/ListEmployeeComponent.vue'),
+  },
+  {
+    path: '/edit-employee/:id',
+    name: 'Update Employee',
+    component: () => import('../components/edit-employee/EditEmployeeComponent.vue'),
   },
 ];
 
